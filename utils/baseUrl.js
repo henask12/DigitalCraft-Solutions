110218ce2b03f831005d15987f555a2b11409f25
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-17 11:42:43 +0300

    updated

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://neemo-react.envytheme.com' 
+? 'https://guramayledigitalsolution.netlify.app/' 
 : 'http://localhost:3000';
 
 export default baseUrl;

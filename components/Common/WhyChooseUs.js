7449a6ca7f474c7f1371d623e3d853be87777103
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-25 20:16:54 +0300

    update why choose us page

--- components/Common/WhyChooseUs.js
+++ components/Common/WhyChooseUs.js
@@ -10,20 +10,38 @@ const WhyChooseUs = () => {
             <div className="col-lg-6 col-md-12">
               <div className="choose-content-area">
                 <span>Why Choose Us</span>
-                <h3>Outstanding Digital Experience</h3>
+                <h3>Our Outstanding Experience</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
-                  maecenas accumsan lacus vel facilisis.{" "}
+                Elevate your journey with a team that boasts a rich history of success and expertise. With a proven track record
+                in delivering exceptional results, we bring unparalleled experience to every project. Explore a partnership
+                that goes beyond expectations, defining new standards in the digital realm.{" "}
                 </p>
 
                 <div className="choose-text">
                   <i className="flaticon-check-mark"></i>
-                  <h4>First Working Process</h4>
+                  <h4>Aviation-Rooted Expertise</h4>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor abore et dolore magna aliqua.
+                  Leveraging Aviation Expertise: Rooted in the aviation industry, our team offers valuable insights tailored to your sector's nuances. 
+                  Benefit from our proven track record collaborating with Ethiopian Airlines, demonstrating our ability to deliver results and navigate industry complexities.
+                  </p>
+                </div>
+
+                <div className="choose-text">
+                  <i className="flaticon-check-mark"></i>
+                  <h4>Comprehensive Digital Prowess</h4>
+                  <p>
+                  Diverse Expertise, Tailored Solutions: Rooted in aviation, we offer nuanced insights and a proven track record with Ethiopian Airlines. 
+                  Our versatile team, spanning digital realms, ensures a one-stop-shop for your needs. 
+                  From creative campaigns to user-centric design, we craft innovative strategies for your distinct presence.
+                  </p>
+                </div>
+
+                <div className="choose-text">
+                  <i className="flaticon-check-mark"></i>
+                  <h4>Client-Centric Partnership</h4>
+                  <p>
+                    Strategic Collaboration for Impact: Partnering closely, we merge aviation insights and a proven track record with a versatile team, offering a unified solution hub. 
+                    From innovative campaigns to user-centric design, we tailor strategies for your brand's distinct success.
                   </p>
                 </div>
 
@@ -31,17 +49,8 @@ const WhyChooseUs = () => {
                   <i className="flaticon-check-mark"></i>
                   <h4>Dedicated Team Member</h4>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor abore et dolore magna aliqua.
-                  </p>
-                </div>
-
-                <div className="choose-text">
-                  <i className="flaticon-check-mark"></i>
-                  <h4>24/7 Support</h4>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor abore et dolore magna aliqua.
+                    Discover the excellence of our team, where each member is dedicated to delivering top-notch solutions with passion and expertise.
+                    Our commitment to your success drives our collaborative efforts, making us the ideal choice for your digital needs.
                   </p>
                 </div>
 
